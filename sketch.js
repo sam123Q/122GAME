2b8864cb471d617201ace861538827956400b35f
bhavik <mprathima25@gmail.com>
2020-12-14 16:46:18 +0000

    added code for constrained body

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var chain, log6;
+
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -18,7 +21,7 @@ function setup(){
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
-
+   
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -35,6 +38,16 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+
+    log6 = new Log(230,180,80, PI/2);
+
+    chain = new Chain(bird.body, log6.body);
+
+
+
+  
+
+
 
 }
 
@@ -61,4 +74,8 @@ function draw(){
 
     bird.display();
     platform.display();
+    log6.display();
+    chain.display();
+
+    
 }
